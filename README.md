3dad49a775979978f1a353fcd2a42f6682660c85
geryy <g@geryy.io>
2021-01-22 14:59:27 +0100

    rc1

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# westlotto-jackpot-scrapper
+# Jackpot Scraper 
+### West Lotto
+
+Example Response
+```json
+{
+  "lottoJackpott": "9 Mio.",
+  "euroJackpott": "10 Mio.",
+  "spiel77": "4 Mio.",
+  "cachedSince": "2021-01-22T13:58:26.924Z"
+}
+```
+
+#### Hosted on Vercel
+cache is set to 5 minutes
---
+++ api/index.ts
@@ -0,0 +1,26 @@
+
+import { NowRequest, NowResponse } from '@vercel/node'
+import fetch from 'node-fetch'
+import cheerio from 'cheerio'
+
+
+module.exports = async (req: NowRequest, res: NowResponse) => {
+
+  const response = await fetch('https://www.westlotto.de/')
+  const htmlString = await response.text()
+
+  const $ = cheerio.load(htmlString)
+
+  const eurojackpot = $('#gametile > div > div:nth-child(1) > div > div.mod-gametile__content > div.mod-gametile__content__hl-container > p.heading-h3.eur-appx-sup').text()
+
+  const lottojackpot = $('#gametile > div > div:nth-child(2) > div > div.mod-gametile__content > div.mod-gametile__content__hl-container > p.heading-h3.eur-appx-sup').text()
+
+  const spiel77 = $('#gametile > div > div:nth-child(3) > div > div.mod-gametile__content > div.mod-gametile__content__hl-container > p.heading-h3.eur-appx-sup').text()
+
+  const cache = 60 * 5 // 5 minuten
+  const cachedSince = new Date()
+
+  res.setHeader('Cache-Control', "s-maxage=" + cache)
+  res.status(200).json({ lottojackpot, eurojackpot, spiel77, cachedSince })
+}
+
